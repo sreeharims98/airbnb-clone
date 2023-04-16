054b7d41f7cf97dfa5d74f2c8c948a240351a364
sreeharims <iamsreeharims@gmail.com>
2023-04-16 11:22:55 +0530

    add: category ui

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,5 +6,14 @@ import { useRouter } from "next/navigation";
 export default function Logo() {
   const router = useRouter();
 
-  return <Image alt="logo" className="hidden md:block cursor-pointer" width={100} height={100} src="/images/logo.png" />;
+  return (
+    <Image
+      alt="logo"
+      className="hidden md:block cursor-pointer"
+      width={100}
+      height={100}
+      src="/images/logo.png"
+      onClick={() => router.push("/")}
+    />
+  );
 }
