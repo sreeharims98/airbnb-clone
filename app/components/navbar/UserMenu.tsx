c35fd6c615b39893be9a3907efa8722879aee2c9
sreeharims <iamsreeharims@gmail.com>
2023-04-29 10:58:49 +0530

    add: property page add: property delete API

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -71,7 +71,10 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
                   onClick={() => router.push("/reservations")}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label="My properties"
+                />
                 <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
